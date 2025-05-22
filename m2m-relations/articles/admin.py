b9d87c2169969ff8294ba423dd80b1ea2d513c18
Ekaterina.Bozunova <katrin060189@gmail.com>
2025-05-23 00:07:47 +0700

    fix remarks

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from .models import Article, Section, Scope
 
-from .models import Article
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_section_count = 0
+        for form in self.forms:
+            if not form.cleaned_data.get('DELETE', False):
+                section = form.cleaned_data.get('section')
+                if section and section.is_main:  # предполагается, что есть поле is_main
+                    main_section_count += 1
+        if main_section_count != 1:
+            raise ValidationError('Должен быть выбран ровно один основной раздел.')
 
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
